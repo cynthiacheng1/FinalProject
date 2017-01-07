af9d2602ed2d19fa45abe3208cf4567a4e7c29ad
Cynthia Cheng <cynthiacheng@Cynthias-MacBook-Air.local>
2017-01-07 13:56:13 -0500

    created two circles with something kinda connecting)

--- ShortestPath.java
+++ ShortestPath.java
@@ -1,74 +1,47 @@
-     
-import java.applet.Applet;
+import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class ShortestPath extends Applet implements Runnable {
-  int frameNumber = -1;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.Timer;
 
-  int delay = 100;
+public class ShortestPath extends JPanel implements ActionListener{
 
-  Thread animatorThread;
+	//gui version of sleep method
+	//Timer tm = new Timer(5, this); //this= actionlistener
+	//int x=0, velX=2;
 
-  boolean frozen = false;
+	public void paintComponent(Graphics g){
+		super.paintComponent(g);
+		g.setColor(Color.RED);
+		g.fillOval(100,100,50,50);
+		g.setColor(Color.RED);
+		g.fillOval(200,50,50,50);
+		//tm.start();
+		g.drawLine(100, 100, 200, 50);
+		//line at angle
 
-  public void init() {
-    String str;
-    addMouseListener(new MouseAdapter() {
-      public void mousePressed(MouseEvent e) {
-        if (frozen) {
-          frozen = false;
-          start();
-        } else {
-          frozen = true;
-          stop();
-        }
-      }
-    });
-  }
+	}
 
-  public void start() {
-    if (!frozen) {
-      if (animatorThread == null) {
-        animatorThread = new Thread(this);
-      }
-      animatorThread.start();
-    }
-  }
+	public void actionPerformed(ActionEvent e) {
+		//repaint();
+	}
 
-  public void stop() {
-    animatorThread = null;
-  }
+	public static void main(String[] args){
+		ShortestPath s = new ShortestPath();
+		JFrame jf = new JFrame();
+		jf.setTitle("Shortest Path Algorithm");
+		jf.setSize(600, 400);
+		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jf.add(s);
+		jf.setVisible(true);
+		s.setLayout(new GridBagLayout());
+		GridBagConstraints cl;
+		cl = new GridBagConstraints();
+		cl.gridy = 0;
+		s.add(new JLabel("Hello"), cl);
+	}
 
-  public void run() {
-    Thread.currentThread().setPriority(Thread.MIN_PRIORITY);
-
-    long startTime = System.currentTimeMillis();
-
-    Thread currentThread = Thread.currentThread();
-
-    while (currentThread == animatorThread) {
-      frameNumber++;
-
-      repaint();
-
-      try {
-        startTime += delay;
-        Thread.sleep(100);
-      } catch (InterruptedException e) {
-        break;
-      }
-    }
-  }
-
-  public void paint(Graphics g) {
-    g.drawString("Frame " + frameNumber, 0, 30);
-  }
-
-  public static void main(String[] args) {
-     ShortestPath g = new ShortestPath();
-     ShortestPath.setVisible(true);
-  }
 }
-      
