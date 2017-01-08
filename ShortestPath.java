90015e9a2310f53eba6522c53c634f694ba1490e
Cynthia Cheng <cynthiacheng@Cynthias-MacBook-Air.local>
2017-01-08 18:47:30 -0500

    GUI not working; J Frame vs JPanel

--- ShortestPath.java
+++ ShortestPath.java
@@ -1,13 +1,37 @@
-import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.Timer;
-
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 public class ShortestPath extends JPanel implements ActionListener{
+	private Container pane;
+    private JLabel j;
+    private JTextField t;
+    int numOfNodes;
+
+   public ShortestPath(){
+
+   }
+   public ShortestPath(int x) {
+   	JFrame j = new JFrame();
+    j.setTitle("Input Your Nodes");
+    j.setSize(600,400);
+    j.setLocation(100,100);
+    j.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    pane = j.getContentPane();
+    pane.setLayout(new BoxLayout(pane, BoxLayout.Y_AXIS));
+    JButton b = new JButton("ByteMe");
+    b.addActionListener(this);
+    b.setActionCommand("Byte");
+    JButton b2 = new JButton("No...");
+    b2.addActionListener(this);
+    b2.setActionCommand("NotByte");
+    t = new JTextField(10);
+    JCheckBox c = new JCheckBox("OverClock");
+    pane.add(c);
+    pane.add(t);
+    pane.add(b);
+    pane.add(b2);
+  }
 
 	//gui version of sleep method
 	//Timer tm = new Timer(5, this); //this= actionlistener
@@ -20,7 +44,8 @@ public class ShortestPath extends JPanel implements ActionListener{
 		g.setColor(Color.RED);
 		g.fillOval(200,50,50,50);
 		//tm.start();
-		g.drawLine(100, 100, 200, 50);
+		//added twenty to everything to move down
+		g.drawLine(120, 120, 220, 70);
 		//line at angle
 
 	}
@@ -30,18 +55,19 @@ public class ShortestPath extends JPanel implements ActionListener{
 	}
 
 	public static void main(String[] args){
-		ShortestPath s = new ShortestPath();
-		JFrame jf = new JFrame();
-		jf.setTitle("Shortest Path Algorithm");
-		jf.setSize(600, 400);
-		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		jf.add(s);
-		jf.setVisible(true);
-		s.setLayout(new GridBagLayout());
-		GridBagConstraints cl;
-		cl = new GridBagConstraints();
-		cl.gridy = 0;
-		s.add(new JLabel("Hello"), cl);
+		ShortestPath y = new ShortestPath(5);
+		y.setVisible(true);
+		int x = 0;
+		if (x == 5) {
+			ShortestPath s = new ShortestPath();
+			JFrame jf = new JFrame();
+			jf.setTitle("Shortest Path Algorithm");
+			jf.setSize(600, 400);
+			jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			jf.add(s);
+			jf.setVisible(true);
+		}
+
 	}
 
 }
