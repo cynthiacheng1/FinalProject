5c9b9479ce705c5bc0eed50bb26535462d550d4a
Cynthia Cheng <cynthiacheng@Cynthias-MacBook-Air.local>
2017-01-15 18:42:28 -0500

    random generator

--- ShortestPathNew.java
+++ ShortestPathNew.java
@@ -5,10 +5,13 @@ import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.Random;
 
-public class ShortestPathNew extends JPanel implements ActionListener{
+public class ShortestPathNew extends JPanel implements ActionListener, ComponentListener{
 
     String info;
     int firstX, firstY, secondX, secondY;
+    int width = 600;
+    int height = 400;
+    ShortestPathNew s;
 
 
 	//gui version of sleep method
@@ -37,6 +40,17 @@ public class ShortestPathNew extends JPanel implements ActionListener{
  		return ans.substring(0,ans.length()) + "]";
     }
 
+    public void componentHidden(ComponentEvent e){}
+    public void componentMoved(ComponentEvent e){}
+    public void componentShown(ComponentEvent e){}
+    public void componentResized(ComponentEvent e){
+    	Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		int w = (int)screenSize.getWidth();
+		int h = (int)screenSize.getHeight();
+		width = w;
+		height = h;
+    }
+
 
 	public void paintComponent(Graphics g){
 		// info = info.substring(0,info.length());
@@ -45,20 +59,22 @@ public class ShortestPathNew extends JPanel implements ActionListener{
 		// 	g.setColor(Color.RED);
 		// 	g.fillOval(100*i,100*i,75,75);
 		// }
+		s = new ShortestPathNew();
 		Random rand = new Random();
-		int numOfNodes = ThreadLocalRandom.current().nextInt(4, 10 + 1);
+		int numOfNodes = ThreadLocalRandom.current().nextInt(4, 9 + 1);
 		System.out.println(numOfNodes);
 		Color[] colors = {Color.red, Color.blue, Color.green, Color.yellow, Color.black, Color.pink, Color.gray};
 		int[][] positions = new int[numOfNodes][2]; 
+
 		for (int i=0; i <numOfNodes; i++){
 			Color culr = colors[i%colors.length];
 			g.setColor(culr);
-			int x = rand.nextInt(550/numOfNodes) * i;
-			int y = rand.nextInt(300);
+			int x = rand.nextInt((width-50)/numOfNodes) * i;
+			int y = rand.nextInt(height-100);
 
 			while(!checkNewCoord(x, y, i, positions)){
-				x = rand.nextInt(550/numOfNodes) * i;
-				y = rand.nextInt(300);
+				x = rand.nextInt((width - 50)/numOfNodes) * i;
+				y = rand.nextInt(height - 100);
 			}
 
 			positions[i][0] = x;
@@ -75,14 +91,14 @@ public class ShortestPathNew extends JPanel implements ActionListener{
 
 			x2 = positions[i+1][0] + 25;
 			y2 = positions[i+1][1] + 25;
-
+			g.setColor(Color.black);
 			g.drawLine(x1,y1,x2,y2);
-			String distance = "" + distanceForm(x1, x2, y1, y2);
+			String distance = "" + (int)distanceForm(x1, x2, y1, y2);
 			char[] label = new char[distance.length()];
 			for(int j = 0; j < distance.length(); j++){
 				label[j] = distance.charAt(j);
 			}
-			g.drawChars(label, 0, 10, ((x1 + x2)/2), ((y1 + y2)/2));
+			g.drawChars(label, 0, label.length, ((x1 + x2)/2), ((y1 + y2)/2));
 		}
 
 		for (int i=0; i <numOfNodes; i++){
@@ -96,6 +112,13 @@ public class ShortestPathNew extends JPanel implements ActionListener{
 			y2 = positions[index2][1] + 25;
 
 			g.drawLine(x1,y1,x2,y2);
+
+			String distance = "" + (int)distanceForm(x1, x2, y1, y2);
+			char[] label = new char[distance.length()];
+			for(int j = 0; j < distance.length(); j++){
+				label[j] = distance.charAt(j);
+			}
+			g.drawChars(label, 0, label.length, ((x1 + x2)/2), ((y1 + y2)/2));
 		}
 		
 	}
@@ -143,7 +166,7 @@ public class ShortestPathNew extends JPanel implements ActionListener{
 			jf.setSize(600, 400);
 			jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			jf.add(s); //adding panel ontop of frame
-			jf.setVisible(true); 
+			jf.setVisible(true);
 
 		}
 	}
